f71a13478159d1af7d8a824fe78f01cc947b3af4
Berry  Semexan <bsemexan@gmail.com>
2018-04-10 06:06:52 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,12 @@ ActiveRecord::Schema.define(version: 20180410051017) do
   end
 
   create_table "preferences", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "allow_create_artists"
+    t.boolean  "allow_create_songs"
+    t.string   "artist_sort_order"
+    t.string   "song_sort_order"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   create_table "songs", force: :cascade do |t|
